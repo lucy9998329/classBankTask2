82b4afb1c419f51bfe20eeed5a58abaf1e4b5519
lucyL <lucylaurentino@gmail.com>
2018-03-19 10:42:19 +1300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace classBankTask2
+{
+class Client 
+{
+    string Username {get; set;}
+
+    string PinNumber {get; set;}
+
+    double Balance {get; set;}
+}
+
+class Bank
+{
+    void WithDrawal(){
+        Console.WriteLine("Logic for a withdrawal");
+    }
+
+}
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Customer!");
+
+            var c1 = new Client();
+            var b1 = new Bank();
+        }
+    }
+}
